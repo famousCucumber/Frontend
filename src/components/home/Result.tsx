76ed364092536241d8b5184ae4a4ae854c4d561e
haeseoklee <haeseoklee.dev@gmail.com>
2021-05-13 22:01:37 +0900

    feat: Result 컴포넌트 구현

--- src/components/home/Result.tsx
+++ src/components/home/Result.tsx
@@ -1,17 +1,76 @@
 import styled from "styled-components";
 import { ITag } from "types";
+import Card from "components/home/Card";
 
 interface ResultProps {
     tags: ITag[];
 }
 const Result = ({ tags }: ResultProps) => {
-    return <StyleSection></StyleSection>;
+    return (
+        <>
+        <StyleSection>
+            <TimeSection>
+                {getCurrentTime()}
+            </TimeSection>
+            <DateSection>
+                {getCurrentDate()}
+            </DateSection>
+            <EmptyBlock/>
+            {tags.map((tag, index) => 
+                    <>
+                    tag.isSelect ? <Card key={`${index} + ${tag.name}`} tag={tag}></Card> : <></>
+                    <EmptyBlock2/>
+                    </>
+                )
+            }
+        </StyleSection>
+        </>
+    );
 };
 
+const getCurrentTime = () => {
+    let today = new Date();
+    const hour = today.getHours();
+    const minute = today.getMinutes().toString().padStart(2, '0');
+    return `${hour}:${minute}`
+}
+
+const getCurrentDate = () => {
+    let today = new Date();
+    const weekdayArray = ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일'];
+    const month = today.getMonth();
+    const day = today.getDate().toString().padStart(2, '0');
+    const weekday = weekdayArray[today.getDay()];
+    return `${month}월 ${day}일 ${weekday}`
+}
+
 export default Result;
+
+const EmptyBlock = styled.section`
+    height: 5%;
+`
+
+const EmptyBlock2 = styled.section`
+    height: 1%;
+`
+
+const TimeSection = styled.section`
+    text-align: center;
+    color: white;
+    font-size: 5em;
+    font-weight: 200;
+`
+
+const DateSection = styled.section`
+    text-align: center;
+    color: white;
+    font-size: 2em;
+    font-weight: 100;
+`
 
 const StyleSection = styled.section`
     width: 50vw;
     height: 100vh;
-    background-color: ${({ theme }) => theme.colors.c100};
+    background-color: black;
+    overflow: scroll;
 `;
