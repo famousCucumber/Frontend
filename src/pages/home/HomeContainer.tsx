d8303f429eabfea6584621f12cf982c104cc902d
hyesung oh <haesungoh414@gmail.com>
2021-05-14 09:19:43 +0900

    fix: fix regex

--- src/pages/home/HomeContainer.tsx
+++ src/pages/home/HomeContainer.tsx
@@ -20,7 +20,8 @@ const HomeContainer = () => {
 
     const onSubmit = async (e: React.FormEvent) => {
         const validateEmail = (testEmail: string) => {
-            const re = /^[^\s@]+@[^\s@]+$/;
+            const re =
+                /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
             return re.test(String(testEmail).toLowerCase());
         };
 
